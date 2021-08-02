cbc72a67744f20e1e53d2f0b74cefe218b82ae68
Rocco Sicilia <rocco.sicilia@protonmail.com>
2021-08-02 21:18:40 +0200

    remove count var

--- getdata/getcve.py
+++ getdata/getcve.py
@@ -47,7 +47,6 @@ for cve in lastcve:
     # check for duplicate CVE
     sql_checkcve = "SELECT * FROM cve WHERE cve_id = '{0}' AND date_modified = '{1}'".format(cve["id"], cve["Modified"])
     cursor.execute(sql_checkcve)
-    cveindb = cursor.rowcount
 
     if cursor.rowcount == 0:
         sql_addcve = "INSERT INTO cve (cve_id, cvss, date_modified, date_published, cpe_list) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')".format(cve["id"], cve["cvss"], cve["Modified"], cve["Published"], cpe_list)
