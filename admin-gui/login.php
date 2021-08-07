bcaeb2a07aff496f51af12d2b9c010b6cbab69c1
Rocco Sicilia <rocco.sicilia@protonmail.com>
2021-08-07 21:53:07 +0200

    login test

--- admin-gui/login.php
+++ admin-gui/login.php
@@ -1,5 +1,16 @@
 <?php
 
-echo "LOGIN PAGE";
+include("./functions.inc.php");
+include("./config.inc.php");
+
+// session check
+if (!isset($session["username"]))
+{
+    tmpl_login($title);
+}
+else
+{
+    echo "reg.";
+}
 
 ?>
