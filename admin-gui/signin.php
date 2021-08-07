0443dc689127e1044f75ad4230120d39ce660e53
Rocco Sicilia <rocco.sicilia@protonmail.com>
2021-08-07 23:11:48 +0200

    fix query

--- admin-gui/signin.php
+++ admin-gui/signin.php
@@ -7,8 +7,8 @@ include("./functions.inc.php");
 if (!isset($session["username"]) AND isset($_POST["login_email"]) AND isset($_POST["login_password"]))
 {
     $username = addslashes(stripslashes($_POST["login_email"]));
-    $res = pg_query($dbconn, "SELECT * FROM users WHERE username = '" . $username . " ORDER BY id");
-    $arr = pg_fetch_array($res);
+    $res = pg_query($dbconn, "SELECT * FROM users WHERE username = '" . $username . "' ORDER BY id");
+    $arr = pg_fetch_assoc($res);
 
     print_r($arr);
 
