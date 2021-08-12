5dbadd9356649ba9dc9187a2250311e8e48464d1
Rocco Sicilia <rocco.sicilia@protonmail.com>
2021-08-13 01:02:48 +0200

    test

--- getdata/setcpe.py
+++ getdata/setcpe.py
@@ -28,7 +28,7 @@ for result in results:
   
   # DEBUG
   if infoj != None:
-    print("cpe: {}".format(infoj["cpe"]))
+    print("cpe: {}".format(infoj))
 
   # DEBUG
   # print("{0} {1}".format(info["cpe"], info["Description"]))
