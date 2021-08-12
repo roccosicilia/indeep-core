b8732e1ac72b4c95abfc028d93a32219b8a22dee
Rocco Sicilia <rocco.sicilia@protonmail.com>
2021-08-12 23:09:37 +0200

    test

--- getdata/setcpe.py
+++ getdata/setcpe.py
@@ -23,6 +23,6 @@ for result in results:
   # DEBUG
   # print("CEV ID: {0}.".format(result[1]))
 
-  info = json.dumps(result[7])
+  info = json.dumps(str(result[7]))
   if info["cpe"] == 'set':
     print("The CVE {0} was managed".formta(result[1]))
