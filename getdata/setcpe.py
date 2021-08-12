1d9344539536685ebc3caa24f3b96bfc3527989e
Rocco Sicilia <rocco.sicilia@protonmail.com>
2021-08-12 22:43:54 +0200

    test cpeset

---
+++ getdata/setcpe.py
@@ -0,0 +1,23 @@
+#
+# File: setcpe.py
+# Author: Rocco Sicilia (aka: Sheliak)
+#
+
+from urllib import urlopen
+import json
+import mysql.connector
+
+# config file
+static = json.load(open("./config.json"))
+
+# DB connection MYSQL
+connection = mysql.connector.connect(host=static["db_host"], user=static["db_user"], password=static["db_password"], database=static["db_name"])
+cursor = connection.cursor()
+
+# get cve whit unset cpe
+sql_cve = "SELECT * FROM `cve` WHERE `info` NOT LIKE '%\"cpe\" : \"set\"%' ORDER BY `id`"
+cursor.execute(sql_cve)
+results = cursor.fetchall()
+
+for result in results:
+  print(result)
