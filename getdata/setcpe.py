60c76c898458f8a4928102adf39eafed1d8a03b3
Rocco Sicilia <rocco.sicilia@protonmail.com>
2021-08-13 01:04:50 +0200

    test

--- getdata/setcpe.py
+++ getdata/setcpe.py
@@ -23,7 +23,7 @@ for result in results:
   # DEBUG
   # print("CEV ID: {0}.".format(result[1]))
 
-  infoj = json.dumps(result[7])
+  infoj = json.dumps(str(result[7]))
   infoj = json.loads(str(infoj))
   
   # DEBUG
