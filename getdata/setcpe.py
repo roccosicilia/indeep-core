9a2335389b591bba24fe27b645868e8ccdb7161f
Rocco Sicilia <rocco.sicilia@protonmail.com>
2021-08-13 09:30:36 +0200

    chande DB structure

--- getdata/setcpe.py
+++ getdata/setcpe.py
@@ -16,13 +16,17 @@ connection = mysql.connector.connect(host=static["db_host"], user=static["db_use
 cursor = connection.cursor()
 
 # get cve whit unset cpe
-sql_cve = "SELECT * FROM `cve` ORDER BY `id`"
+sql_cve = "SELECT * FROM `cve` WHERE `cpe` != Null AND `cpe_set` = Null ORDER BY `id`"
 cursor.execute(sql_cve)
 results = cursor.fetchall()
 
 for result in results:
+  print(result)
+
+'''
   if result[7] != None:
     info = json.loads(result[7])[0]
     if info['cpe'] != 'set':
       cpe_list = result[5].split(",")
       print(cpe_list)
+'''
