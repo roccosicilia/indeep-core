846fbb7363f06bb03c3412a06ab0272bb1b8d5e5
Rocco Sicilia <rocco.sicilia@protonmail.com>
2021-08-13 02:35:29 +0200

    test

--- getdata/setcpe.py
+++ getdata/setcpe.py
@@ -21,7 +21,6 @@ cursor.execute(sql_cve)
 results = cursor.fetchall()
 
 for result in results:
-  if result[7] != None:
-    info = json.loads(result[7])[0]
-    if info['cpe'] != 'set':
-      print(result[5])
+  info = json.loads(result[7])[0]
+  if info['cpe'] != 'set':
+    print(result[5])
