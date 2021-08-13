4200d3653f940f9784749064f36f8ff6fb2485cf
Rocco Sicilia <rocco.sicilia@protonmail.com>
2021-08-13 02:15:54 +0200

    test

--- getdata/setcpe.py
+++ getdata/setcpe.py
@@ -19,9 +19,6 @@ cursor = connection.cursor()
 sql_cve = "SELECT * FROM `cve` ORDER BY `id`"
 cursor.execute(sql_cve)
 results = cursor.fetchall()
-data = json.dumps(results)
-info = json.loads(data)
 
-for a in info:
-  if a != None:
-    print(a['cpe'])
+for result in results:
+  print(result)
