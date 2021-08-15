b197ccbde162185480526cc53a7966e0d2fad7a1
Rocco Sicilia <rocco.sicilia@protonmail.com>
2021-08-15 11:51:17 +0200

    fix query

--- getdata/alertvuln.py
+++ getdata/alertvuln.py
@@ -33,7 +33,7 @@ for istance in istances:
     for cpe in cpe_list:
         print("## Identified cpe {}:{}".format(cpe[3], cpe[4]))
         # check vuln for cpe_list
-        sql_cve = "SELECT * FROM `cve` WHERE `cpe_list` LIKE '{0}:{1}' ORDER BY `id`".format(cpe[3], cpe[4])
+        sql_cve = "SELECT * FROM `cve` WHERE `cpe_list` LIKE '%{0}:{1}%' ORDER BY `id`".format(cpe[3], cpe[4])
         cursor.execute(sql_cve)
         cve_list = cursor.fetchall()
         
