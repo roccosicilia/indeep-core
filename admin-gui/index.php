ff10a092b8efea9513238f729a2d22d39e64ce12
Rocco Sicilia <rocco.sicilia@protonmail.com>
2021-08-17 19:21:43 +0200

    fix

--- admin-gui/index.php
+++ admin-gui/index.php
@@ -24,7 +24,7 @@ echo "<div class=\"col-lg-12 grid-margin stretch-card\">\n";
 echo "<div class=\"card\">\n";
 echo "<div class=\"card-body\">\n";
 
-echo "<canvas id=\"line-chart\" style=\"width: 90vw; height: 30vh\"></canvas>\n";
+echo "<canvas id=\"line-chart\" style=\"width: 90wv; height: 30hv;\"></canvas>\n";
 
 echo "</div>\n";
 echo "</div>\n";
